21274aa90c50c87427e4706f29b3bb9dfecff60c
Steven Lok <loksteven0116@gmail.com>
2023-09-07 01:47:37 -0700

    remove ssl requirement

--- src/app/api/_db/index.ts
+++ src/app/api/_db/index.ts
@@ -6,11 +6,11 @@ export const pool = new pg.Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-  ssl:
-    process.env.DB_HOST !== 'localhost'
-      ? {
-          ca: require('fs').readFileSync('@/../ssl-certs/us-east-1-bundle.pem'),
-          rejectUnauthorized: true, // Set to true to enforce SSL verification
-        }
-      : undefined,
+  // ssl:
+  //   process.env.DB_HOST !== 'localhost'
+  //     ? {
+  //         ca: require('fs').readFileSync('@/../ssl-certs/us-east-1-bundle.pem'),
+  //         rejectUnauthorized: true, // Set to true to enforce SSL verification
+  //       }
+  //     : undefined,
 });
