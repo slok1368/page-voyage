16aade5d92d295ff336ca2adf0c33d1f187afc95
Steven Lok <loksteven0116@gmail.com>
2023-09-08 22:25:12 -0700

    Config change to fix Amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    return config;
+  },
   experimental: {
     serverActions: true,
   },
